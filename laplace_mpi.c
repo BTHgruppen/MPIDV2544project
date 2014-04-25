c0b1ceaaad5de0d7643490f465e4648370bf292a
Mattias Willemsen <thehippiecoder@gmail.com>
2014-04-25 15:26:46 +0200

    Added TODO

--- laplace_mpi.c
+++ laplace_mpi.c
@@ -1,1 +1,14 @@
-#define private public
+
+int main(int argc, char **argv)
+{
+	/*
+	1. Generate matrix
+	2. Divide into blocks
+	3. Send blocks to other nodes
+	4. Each node calculates their block, row by row
+	5. Exchange row values with adjacent blocks (Note: Use Sendreceive to avoid deadlock!)
+	6. Check acceptance value, if we have not passed it yet, goto 4
+	7. If we pass the acceptance value, add everything together into one matrix
+	*/
+}
+
