a3201c8e487cb97193c147d45b547cb307cce2dc
hazardus <axelsson.92@live.se>
2014-04-27 19:46:37 +0200

    Added PrintMatrix()

--- laplace_mpi.c
+++ laplace_mpi.c
@@ -14,6 +14,24 @@
 
 static double A[SIZEWITHBORDERS][SIZEWITHBORDERS];
 
+void InitializeMatrix();
+void PrintMatrix();
+
+int main(int argc, char **argv)
+{
+	// Generate the matrix.
+	InitializeMatrix();
+
+	/* TODO
+	2. Divide into blocks
+	3. Send blocks to other nodes
+	4. Each node calculates their block, row by row
+	5. Exchange row values with adjacent blocks (Note: Use Sendreceive to avoid deadlock!)
+	6. Check acceptance value, if we have not passed it yet, goto 4
+	7. If we pass the acceptance value, add everything together into one matrix
+	*/
+}
+
 void InitializeMatrix()
 {
 	// If we are in debug mode, print matrix definitions.
@@ -123,22 +141,20 @@ void InitializeMatrix()
 	}
 }
 
-int main(int argc, char **argv)
+void PrintMatrix()
 {
-	// Generate the matrix.
-	InitializeMatrix();
+	int i, j;
 
-	/* TODO
-	2. Divide into blocks
-	3. Send blocks to other nodes
-	4. Each node calculates their block, row by row
-	5. Exchange row values with adjacent blocks (Note: Use Sendreceive to avoid deadlock!)
-	6. Check acceptance value, if we have not passed it yet, goto 4
-	7. If we pass the acceptance value, add everything together into one matrix
+	// Iterate through the matrix and print it.
+    for (i = 0; i < SIZEWITHBORDERS ;i++)
+	{
+		for (j = 0; j < SIZEWITHBORDERS ;j++)
+		{
+			printf(" %f", A[i][j]);
+		}
+		
+		printf("\n");
+	}
 
-	Add print matrix function.
-	*/
+    printf("\n\n");
 }
-
-
-
